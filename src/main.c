2ae8825159eda0c7412e078e856bb915c3cc2de6
Yanay Goor <yanay.goor@gmail.com>
2022-10-15 11:30:16 -0700

    refactor code and add checkout helpers

--- src/main.c
+++ src/main.c
@@ -8,72 +8,135 @@
 #include <sys/queue.h>
 #include <curses.h>
 
-#define CHECKOUT ("checkout:")
+#define REFLOG_CO_PREFIX "checkout:"
+
+#define CHECKOUT_MAX_LEN 100
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+
 
 struct ref {
   char *name;
+  size_t index;
   LIST_ENTRY(ref) entry;
 };
 
 LIST_HEAD(refs, ref);
 
-int get_latest_refs(struct refs *out, git_repository *repo, size_t max) {
+
+bool is_checkout_reflog(const git_reflog_entry *entry) {
+  const char *message = git_reflog_entry_message(entry);
+  return !strncmp(message, REFLOG_CO_PREFIX, strlen(REFLOG_CO_PREFIX));
+}
+
+const char *get_checkout_reflog_target(const git_reflog_entry *entry) {
+  const char *message = git_reflog_entry_message(entry);
+  return strrchr(message, ' ') + 1;
+}
+
+struct ref *create_ref(const char *target, int index) {
+  struct ref *ref = malloc(sizeof(struct ref));
+  ref->index = index;
+  ref->name = malloc(strlen(target) + 1);
+  strcpy(ref->name, target);
+  return ref;
+}
+
+void free_ref(struct ref *ref) {
+  free(ref->name);
+  free(ref);
+}
+
+bool refs_append_unique(struct refs *list, struct ref *ref) {
+  struct ref *curr = LIST_FIRST(list);
+  struct ref *last = curr;
+
+  LIST_FOREACH(curr, list, entry) {
+    if (strcmp(curr->name, ref->name) == 0)
+      return false;
+    last = curr;
+  }
+
+  if (last == NULL) {
+    LIST_INSERT_HEAD(list, ref, entry);
+  } else {
+    LIST_INSERT_AFTER(last, ref, entry);
+  }
+  return true;
+}
+
+void get_latest_refs(struct refs *out, git_repository *repo, size_t max) {
   git_reflog *reflog;
-  struct ref *curr;
-  size_t printed = 0;
+  size_t collected = 0;
 
   git_reflog_read(&reflog, repo, "HEAD");
   size_t count = git_reflog_entrycount(reflog);
-  for (size_t i = 0; i < count && printed < max; i++) {
-    const git_reflog_entry *entry = git_reflog_entry_byindex(reflog, i);
-    const char *message = git_reflog_entry_message(entry);
 
-    if (strncmp(message, CHECKOUT, sizeof(CHECKOUT) - 1)) {
-      continue;
+  for (size_t index = 0; index < count; index++) {
+    const git_reflog_entry *entry = git_reflog_entry_byindex(reflog, index);
+
+    if (!is_checkout_reflog(entry)) continue;
+
+    const char *target = get_checkout_reflog_target(entry);
+    if (refs_append_unique(out, create_ref(target, index))) {
+     collected++;
     }
 
-    const char *ref_name = strrchr(message, ' ') + 1;
-    struct ref *ref = malloc(sizeof(struct ref));
-
-    ref->name = malloc(strlen(ref_name) + 1);
-    memset(ref->name, '\0', strlen(ref_name) + 1);
-    strncpy(ref->name, ref_name, strlen(ref_name));
-
-    if (LIST_EMPTY(out)) {
-     LIST_INSERT_HEAD(out, ref, entry);
-     printed++;
-     continue;
-    }
-    LIST_FOREACH(curr, out, entry) {
-      if (strcmp(curr->name, ref->name) == 0)
-        break;
-      if (LIST_NEXT(curr, entry) == NULL) {
-        LIST_INSERT_AFTER(curr, ref, entry);
-      }
-    }
+    if (collected == max) break;
   }
-  return 0;
+
+  git_reflog_free(reflog);
 }
 
 void clear_refs(struct refs *refs) {
-  while(!LIST_EMPTY(refs)) {
-    struct ref *first = LIST_FIRST(refs);
+  struct ref *first;
+  while ((first = LIST_FIRST(refs)) != NULL) {
     LIST_REMOVE(first, entry);
-    free(first->name);
-    free(first);
+    free_ref(first);
+  }
+}
+
+size_t get_refs_max_width(struct refs *refs) {
+  struct ref *curr;
+  size_t max = 0;
+  LIST_FOREACH(curr, refs, entry) {
+    max = MAX(max, strlen(curr->name));
+  }
+  return max;
+}
+
+void get_co_command(char *out, size_t maxlen, size_t index) {
+  if (index == 1) {
+    strcpy(out, "git checkout -");
+  } else {
+    snprintf(out, maxlen, "git checkout @{-%ld}", index);
   }
 }
 
 void print_refs(WINDOW *win, struct refs *refs) {
   struct ref *curr;
   size_t line = 0;
+  char buff[CHECKOUT_MAX_LEN];
+  size_t max = get_refs_max_width(refs);
 
   LIST_FOREACH(curr, refs, entry) {
-    wmove(win, line++, 0);
+    wmove(win, line, 0);
     wclrtoeol(win);
     waddstr(win, curr->name);
+    if (curr->index) {
+      wmove(win, line, max + 1);
+      wattr_on(win, WA_DIM, NULL);
+      get_co_command(
+          buff,
+        CHECKOUT_MAX_LEN,
+        curr->index
+      );
+      waddstr(win, buff);
+      wattr_off(win, WA_DIM, NULL);
+    }
+    line++;
   }
-  refresh();
+  wrefresh(win);
 }
 
 int main() {
@@ -94,9 +157,9 @@ int main() {
   git_repository_init(&repo, cwd, false);
 
   while (1) {
-    clear_refs(&refs);
     get_latest_refs(&refs, repo, getmaxx(win));
     print_refs(win, &refs);
+    clear_refs(&refs);
     usleep(10000);
   }
 
